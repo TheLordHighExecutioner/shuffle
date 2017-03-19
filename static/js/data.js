df99fbfd8e726dd1d39736ea6eb0327cbe7facb7
Yuzhou Huang <happyjoe2008@gmail.com>
2017-03-19 10:16:00 +0000

    Challenge: don't use jquery

--- static/js/data.js
+++ static/js/data.js
@@ -15,3 +15,4 @@ var playlists = [
       "max": 100
     }
 ];
+
--- static/js/script.js
+++ static/js/script.js
@@ -1,30 +1,23 @@
-import $ from 'jquery';
-
-var generateRandom = function(num) {
-    return Math.floor(Math.random() * num);
+var generateRandom = function (num) {
+  return Math.floor(Math.random() * num);
 };
 
-String.prototype.format = function() {
-    var string = this;
-    for (var i = 0; i < arguments.length; i++) {
-        var regexp = new RegExp("\\{"+i+"\\}", "gi");
-        string = string.replace(regexp, arguments[i]);
-    }
+String.prototype.format = function () {
+  var string = this;
+  for (var i = 0; i < arguments.length; i++) {
+    var regexp = new RegExp("\\{" + i + "\\}", "gi");
+    string = string.replace(regexp, arguments[i]);
+  }
 
-    return string;
+  return string;
 };
 
-let changeSong = function changeSong() {
-  var numOfPlaylists = playlists.length;
-  var loc = generateRandom(numOfPlaylists);
-  var playlist = playlists[loc];
-  var index = generateRandom(playlist.max);
-  var baseURL = "https://www.youtube.com/embed/?list={0}&index={1}";
-  var url = baseURL.format(playlist.id, index);
-  var queryStrings = "&amp;t=15&amp;wmode=transparent&amp;autoplay=1&amp;rel=0&amp;showinfo=0&amp;iv_load_policy=3&amp;showsearch=0&amp;autohide=1&amp;controls=0&amp;wadsworth=1";
-  var iframeSrc = url + queryStrings;
+var numOfPlaylists = playlists.length;
+var loc = generateRandom(numOfPlaylists);
+var playlist = playlists[loc];
+var index = generateRandom(playlist.max);
+var baseURL = "https://www.youtube.com/embed/?list={0}&index={1}";
+var url = baseURL.format(playlist.id, index);
+var queryStrings = "&amp;t=15&amp;wmode=transparent&amp;autoplay=1&amp;rel=0&amp;showinfo=0&amp;iv_load_policy=3&amp;showsearch=0&amp;autohide=1&amp;controls=0&amp;wadsworth=1";
 
-  document.getElementById("player").src = iframeSrc;
-}
-
-$(document)
+document.getElementById("player").src = url + queryStrings;
