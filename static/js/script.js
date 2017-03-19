be5a8353b46898287ee271eecfc6f6b8cc0fda51
Yuzhou Huang <happyjoe2008@gmail.com>
2017-03-19 10:04:26 +0000

    Jquery included

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,5 @@
+import $ from 'jquery';
+
 var generateRandom = function(num) {
     return Math.floor(Math.random() * num);
 };
@@ -12,13 +14,17 @@ String.prototype.format = function() {
     return string;
 };
 
-var numOfPlaylists = playlists.length;
-var loc = generateRandom(numOfPlaylists);
-var playlist = playlists[loc];
-var index = generateRandom(playlist.max);
-var baseURL = "https://www.youtube.com/embed/?list={0}&index={1}";
-var url = baseURL.format(playlist.id, index);
-var queryStrings = "&amp;t=15&amp;wmode=transparent&amp;autoplay=1&amp;rel=0&amp;showinfo=0&amp;iv_load_policy=3&amp;showsearch=0&amp;autohide=1&amp;controls=0&amp;wadsworth=1";
-var iframeSrc = url + queryStrings;
+let changeSong = function changeSong() {
+  var numOfPlaylists = playlists.length;
+  var loc = generateRandom(numOfPlaylists);
+  var playlist = playlists[loc];
+  var index = generateRandom(playlist.max);
+  var baseURL = "https://www.youtube.com/embed/?list={0}&index={1}";
+  var url = baseURL.format(playlist.id, index);
+  var queryStrings = "&amp;t=15&amp;wmode=transparent&amp;autoplay=1&amp;rel=0&amp;showinfo=0&amp;iv_load_policy=3&amp;showsearch=0&amp;autohide=1&amp;controls=0&amp;wadsworth=1";
+  var iframeSrc = url + queryStrings;
 
-document.getElementById("player").src = iframeSrc;
+  document.getElementById("player").src = iframeSrc;
+}
+
+$(document)
